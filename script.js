6a60a21adfe4557671262b971c372cdc45e21aeb
Pavel <mindalev77@mail.ru>
2021-09-22 14:22:19 +0300

    home1

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ const goods = [
     { title: 'Jacket', price: 350 },
     { title: 'Shoes', price: 250 },
 ];
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3 class="goods-title">${title}</h3><p class="goods-price">Price: ${price} $</p> <button class="goods-basket">Add to Cart</button> </div>`;
-};
+const renderGoodsItem = (title, price) =>
+    `<div class="goods-item"><h3 class="goods-title">${title}</h3><p class="goods-price">Price: ${price} $</p> <button class="goods-basket">Add to Cart</button> </div>`;
 const renderGoodsList = (list) => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
     document.querySelector('.goods-list').innerHTML = goodsList.join(" ");
